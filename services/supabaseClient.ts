18f79e543df77d4f3114a3537ec281a2bc3e4ec8
STGDragan <70173272+STGDragan@users.noreply.github.com>
2025-12-13 15:32:13 -0600

    update supabase secret key

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://psrofmaojlttfyrsarrc.supabase.co';
-const supabaseAnonKey = 'seyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzcm9mbWFvamx0dGZ5cnNhcnJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMjc0NTIsImV4cCI6MjA4MDgwMzQ1Mn0.ewf5NP2-58aJL0Ucj_m2G0FVQU88GsKRRFRG9eriDa4';
+const supabaseAnonKey = 'sb_secret_O3v9_48MTS6tL8CuQUo2Dg_4jbJ1hJK';
 
 export const supabase = createClient(
   supabaseUrl,
